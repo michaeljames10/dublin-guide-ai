53d02cf62a541bcf2c5439c46dc73bffe7e18661
michaeljames10 <michaelcjames10@gmail.com>
2025-05-28 23:38:35 +0100

    adjust padding top on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import VisitedAreas from "@/components/visitedAreas";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pt-8 pb-20 gap-16 sm:p-10 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pt-4 pb-20 gap-16 sm:p-10 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start w-full max-w-xl">
         <Image
           aria-hidden
